2edb09a23dfef30c3b7849d36c5d0d123c7abbe0
bianque <bianque@qq.com>
2018-11-15 18:50:19 +0800

    ...

--- test/auth.js
+++ test/auth.js
@@ -12,18 +12,15 @@ describe('认证', function() {
      * 一个单元测试，可使用 skip only 修饰
      * 和负载均衡相关的单元测试，首先连接9901端口，发送config.getServerInfo请求，携带 "stype":"IOS", "oemInfo":{"openid":'helloworl'} 等参数，返回值：data.newbie:是否新注册用户 data.ip:服务器IP, data.port:服务器端口号
      */
-    it.only('注册并登录 - 自动负载均衡'/*单元测试的标题*/, /*单元测试的函数体，书写测试流程*/done =>{
-        remote.auth({openid: `${Math.random()*1000000000 | 0}`}, msg => {
-            remote.isSuccess(msg); //使用断言，对返回值进行合理性判定，如判定失败则抛出异常，下面的 done 就不会被执行
-            done();
-        });
+    it('注册并登录 - 自动负载均衡'/*单元测试的标题*/, /*单元测试的函数体，书写测试流程*/ async () => {
+        let msg = await remote.login({openid: `${Math.random()*1000000000 | 0}`});
+        remote.isSuccess(msg);
     });
 
-    it('路由基准测试', done=>{
-        remote.auth({directly:true}).fetch({}, msg=>{
-            console.log(msg);
-            done();
-        }, `test/ping.html`);
+    it('路由基准测试', async () => {
+        await remote.login();
+        let msg = await remote.fetching({url: `test/ping.html`});
+        console.log(msg);
     });
 
     it.skip('登录 - 自动推送好友列表', done =>{
@@ -44,19 +41,13 @@ describe('认证', function() {
     /**
      * NotifyType.action会在登录时或者冲关体力不足时自动下发,也可以调用 gate.checkAction 主动查询
      */
-    it.only('登录 - 自动推送体力值', done =>{
-        let first = true;
-        remote.watch(msg=>{
-            //服务端下发当前体力、体力上限、下一点体力恢复时间戳
+    it('登录 - 自动推送体力值', async () => {
+        let msg = await remote.watch(msg => { //服务端下发当前体力、体力上限、下一点体力恢复时间戳
             remote.log(msg);
-            if(first){
-                first = false;
-                done();
-            }
-        }, NotifyType.action)
-        .auth({directly:true}, msg => {
-            remote.isSuccess(msg,true);
-            console.log(`用户昵称:${decodeURIComponent(msg.data.name)}`); //中文解码
-        });
+        }, NotifyType.action).login();
+
+        if(remote.isSuccess(msg)) {
+            console.log(`用户昵称: ${decodeURIComponent(msg.data.info.name)}`); //中文解码
+        }
     });
 });
--- test/pressure.js
+++ test/pressure.js
@@ -7,12 +7,12 @@
 let remote = require('./util')
 let {wait} = require('./utils/commonFunc');
 
-describe.skip('压力测试',  function() {
-    it('同时在线', async done =>{
+describe('压力测试',  function() {
+    it('同时在线', async () => {
         let dl = ["tx.IOS", "tx.Android"];
         let j = 0;
-        while(j++< 5000){
-            remote.newone.auth({domain: dl.randObj(), openid:`${remote.config.auth.openid}.${j}`}, msg=>{
+        while(j++ < 5000) {
+            remote.newone.login({domain: dl.randObj(), openid: `${remote.config.auth.openid}.${j}`}).then(msg=>{
                 remote.isSuccess(msg);
                 console.log(j);
             });
@@ -21,10 +21,9 @@ describe.skip('压力测试',  function() {
             await wait(50);
         }
         await wait(5000);
-        done();
     });
 
-    it('并发登录', async function(done) {
+    it.only('并发登录', async () => {
         let presure = [0, 100]; //设置压力参数: [循环次数,单次并发]
 
         //region Promise模式：
@@ -40,9 +39,9 @@ describe.skip('压力测试',  function() {
                 let openid = `${remote.config.auth.openid}.${i+280000}`;
                 //提交登录请求
                 list.push(
-                    new Promise(resolve=>{ 
+                    new Promise(resolve => { 
                         let _rm = remote.newone;
-                        _rm.auth({domain: dl.randObj(), openid:openid}, ()=>{
+                        _rm.login({domain: dl.randObj(), openid:openid}).then(msg => {
                             resolve(_rm.userInfo.openid);
                         });
                     })
@@ -66,8 +65,6 @@ describe.skip('压力测试',  function() {
             //等待一段时间后再执行下一轮
             await wait(3000);
         }
-
-        done();
 
         //endregion
     });
